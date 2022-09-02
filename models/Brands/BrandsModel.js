f81e974ae85b8c84b7435f8fd8e8997f876c997c
Rajdev-ullash <rajdevullash111@gmail.com>
2022-09-02 13:21:07 +0600

    Brands Controller created

---
+++ models/Brands/BrandsModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const DataSchema = new mongoose.Schema(
+  {
+    UserEmail: {
+      type: String,
+    },
+    Name: {
+      type: String,
+      unique: true,
+    },
+    CreatedDate: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  { versionKey: false }
+);
+
+const BrandsModel = mongoose.model("brands", DataSchema);
+module.exports = BrandsModel;
